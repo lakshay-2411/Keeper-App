c9edc2f84af0eb7883807d2e9dfe1483d523aa4d
lakshay-2411 <lakshaynandwani06@gmial.com>
2024-07-28 18:11:48 +0530

    Fixed Header & Notes styling

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Note() {
     return (
-        <div className="bg-white rounded-lg w-60 shadow-md p-2.5 m-4 float-left">
+        <div className="bg-white rounded-lg w-60 shadow-md p-2.5 m-4 float-left ml-8">
             <h1 className="text-lg mb-1.5 font-semibold text-[1.1em]">This is the note title</h1>
             <p className="txt-lg mb-2.5 whitespace-pre-wrap break-words text-[1.1em]">This is the note content</p>
         </div>
